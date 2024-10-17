96cfc719c6503f785d506edb1e1a02cf3e98358e
Alcein <92677997+Alcein@users.noreply.github.com>
2024-10-17 18:25:06 +0800

    Update item_retrieval_agent.py

--- executor_agents/item_retrieval_agent.py
+++ executor_agents/item_retrieval_agent.py
@@ -119,7 +119,7 @@ class ItemRetrievalAgent(Agent):
 
     def recommend_projects_with_reranker(self, user_query, top_n_projects, top_k=10, device='cpu'):
         project_texts = top_n_projects['project_info'].tolist()
-        model_checkpoint = 'bge-reranker-base'
+        model_checkpoint = 'BAAI/bge-large-en-v1.5'
         tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
         model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
         model.eval()
